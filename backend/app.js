c39c5c3331ea5006d64646fe3602450dfdadbef4
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-01-10 14:47:45 +0530

    app.js minor changes

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,12 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle Preflight Requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
   next();
 });
 
